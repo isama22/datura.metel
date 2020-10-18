ccf49562156448934ec94afdf65aa8e0caab5862
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-10-18 02:27:45 -0700

    created and used partials on my one home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Add the following import
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Define the home view
 # def home(request):
 #   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 def home(request):
-  return render(request, 'home.html')
+  return render(request, 'home.html') 
