819ffd5ff91ed69099ff59fb88b9b012b72755aa
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-10-21 18:21:09 -0700

    created post model and first post through admin portal, displayed on index

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 
 # Add the following import
 # from django.http import HttpResponse
@@ -7,4 +8,11 @@ from django.shortcuts import render
 # def home(request):
 #   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 def home(request):
-  return render(request, 'home.html') 
+  posts = Post.objects.all()
+  return render(request, 'home.html', {'posts': posts}) 
+
+def about(request):
+  return render(request, 'about.html')  
+
+def post(request):
+  return render(request, 'post.html')  
