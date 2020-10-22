b4fefd7f7210a4b62b3ffd66309df4d8db35ab78
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-10-21 18:59:40 -0700

    details page set up

--- main_app/views.py
+++ main_app/views.py
@@ -11,8 +11,13 @@ def home(request):
   posts = Post.objects.all()
   return render(request, 'home.html', {'posts': posts}) 
 
+def posts_detail(request, post_id):
+  post = Post.objects.get(id=post_id)
+  return render(request, 'posts/detail.html', { 'post': post })
+
 def about(request):
   return render(request, 'about.html')  
 
 def post(request):
   return render(request, 'post.html')  
+
