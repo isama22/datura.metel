b6efd188e325122b08b02e5fcc01844a55be0644
isama22 <emarti17@artic.edu>
2020-11-14 17:19:42 -0600

    changed title to name in post model for admin portal

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
