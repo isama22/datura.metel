0f440198724f9b24a1abe7f79cc2fc998d6f87e1
isama22 <emarti17@artic.edu>
2020-11-22 00:28:27 -0600

    commented out profile class from models and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Post, Photo, Profile
+from .models import Post, Photo
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import CommentForm
 # Add the following import
