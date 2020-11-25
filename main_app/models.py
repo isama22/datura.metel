a3fa9324db84e39d179d3055b2685ac3614f54d8
isama22 <emarti17@artic.edu>
2020-11-24 22:55:28 -0600

    changed comment input from text area to char field

--- main_app/models.py
+++ main_app/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 class Comment(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
-  text = models.TextField(max_length=100)
+  text = models.CharField(max_length=100)
 
   def __str__(self):
     return self.text
