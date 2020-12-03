a3dac4e2a80fe65f890ddca4a8ce84885ab9d398
isama22 <emarti17@artic.edu>
2020-12-03 16:31:04 -0600

    readding comment model

--- main_app/models.py
+++ main_app/models.py
@@ -32,7 +32,19 @@ class Post(models.Model):
 
 #   def get_absolute_url(self):
 #     return reverse('detail', kwargs={'post_id': self.id}) 
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
 
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
 
 class Photo(models.Model):
     url = models.CharField(max_length=200)
